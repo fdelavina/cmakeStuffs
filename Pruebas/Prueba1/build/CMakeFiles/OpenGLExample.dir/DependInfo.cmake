53e3760a63efd5e588d7a025ad8f81cddd3208e5
fdelavina <77991719+fdelavina@users.noreply.github.com>
2022-03-02 02:12:30 -0800

    opengl pruebas
    pruebas de opengl y glfw

---
+++ Pruebas/Prueba1/build/CMakeFiles/OpenGLExample.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/franhisco/Desktop/openGLudemy/Pruebas/Prueba1/src/main.cpp" "/home/franhisco/Desktop/openGLudemy/Pruebas/Prueba1/build/CMakeFiles/OpenGLExample.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Pruebas/Prueba1/build/CMakeFiles/OpenGLExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLExample.dir/src/main.cpp.o"
+  "OpenGLExample.pdb"
+  "OpenGLExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
