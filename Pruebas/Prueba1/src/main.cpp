ddde27e6ae2b85789bffe754ff500531012926f2
root <frjune10@gmail.com>
2022-03-07 04:33:38 -0800

    terminado prueba1 con triangulo y fps

--- Pruebas/Prueba1/src/main.cpp
+++ Pruebas/Prueba1/src/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 #include "GLFW/glfw3.h"
+#include <sstream>
+
+float lastTime;
+int nbFrames;
+void showFPS(GLFWwindow *pWindow)
+{
+    // Measure speed
+     double currentTime = glfwGetTime();
+     double delta = currentTime - lastTime;
+     nbFrames++;
+     if ( delta >= 1.0 ){ // If last cout was more than 1 sec ago
+         //std::cout << 1000.0/double(nbFrames) << std::endl;
+
+         double fps = double(nbFrames) / delta;
+
+         std::stringstream ss;
+
+         ss << " [" << fps << " FPS]";
+
+         glfwSetWindowTitle(pWindow, ss.str().c_str());
+
+         nbFrames = 0;
+         lastTime = currentTime;
+     }
+}
 
 int main()
 {
@@ -27,8 +52,28 @@ int main()
     {
         glClearColor(0.8, 1.0, 1.0, 0); //fill screen with a color (r,g,b,alpha). 0.0 to 1.0
         glClear(GL_COLOR_BUFFER_BIT); // clean the screen buffer thanks to GL_COLOR_BUFFER_BIT. In OpenGL exists others buffers.
+
+        //draw triangle
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(1.0f,0.0f,0.0f);
+        glVertex3f(-0.5f,-0.4f,0.0f);
+
+        glColor3f(1.0f,0.0f,0.0f);
+        glVertex3f(-0.5f,0.4f,0.0f);
+
+        glColor3f(1.0f,0.0f,0.0f);
+        glVertex3f(0.5f,-0.4f,0.0f);
+
+        glEnd();
+
         glfwSwapBuffers(window); // presents the render frames to actual window
         glfwPollEvents(); // to process the current events
+
+
+        showFPS (window);
+
+        
     }
 
     glfwTerminate();
